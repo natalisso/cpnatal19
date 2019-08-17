524a033351aecd9abe6459b8959f03d97a8cb303
Genilson Filho <jose.genilson@evolux.net.br>
2019-08-17 00:58:12 -0300

    teste

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: uwsgi --http-socket :3031 --plugin python --wsgi-file wsgi.py --callable app -H ../virtuais/cpnatal
+web: uwsgi --ini uwsgi.ini
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,9 @@
-from teste import app
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
